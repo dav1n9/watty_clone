192ee4c4bc31273711094239fe3e0e1b89b4ca20
Davin <85967571+dav1n9@users.noreply.github.com>
2023-05-08 15:51:52 +0900

    더미데이터 추가하여 이미지 띄움.

--- lib/screens/record_screen.dart
+++ lib/screens/record_screen.dart
@@ -1,50 +1,118 @@
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'dart:math';
-
-List<String> imageList = [
-  'https://cdn.pixabay.com/photo/2019/03/15/09/49/girl-4056684_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2020/12/15/16/25/clock-5834193__340.jpg',
-  'https://cdn.pixabay.com/photo/2020/09/18/19/31/laptop-5582775_960_720.jpg',
-  'https://media.istockphoto.com/photos/woman-kayaking-in-fjord-in-norway-picture-id1059380230?b=1&k=6&m=1059380230&s=170667a&w=0&h=kA_A_XrhZJjw2bo5jIJ7089-VktFK0h0I4OWDqaac0c=',
-  'https://cdn.pixabay.com/photo/2019/11/05/00/53/cellular-4602489_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2017/02/12/10/29/christmas-2059698_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2020/01/29/17/09/snowboard-4803050_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2020/02/06/20/01/university-library-4825366_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2020/11/22/17/28/cat-5767334_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2020/12/13/16/22/snow-5828736_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2020/12/09/09/27/women-5816861_960_720.jpg',
-];
 
 class RecordScreen extends StatelessWidget {
   RecordScreen({super.key});
 
-  // Generate a list of dummy items
-  final List<Map<String, dynamic>> _items = List.generate(
-      200,
-      (index) => {
-            "id": index,
-            "title": "Item $index",
-            "height": Random().nextInt(150) + 50.5
-          });
+  // dummy items
+  final List<Map<String, dynamic>> _items2 = [
+    {
+      "id": 0,
+      "title": "추워용❄️",
+      "image":
+          'https://cdn.pixabay.com/photo/2019/03/15/09/49/girl-4056684_960_720.jpg',
+    },
+    {
+      "id": 1,
+      "title": "Item 1",
+      "image":
+          'https://cdn.pixabay.com/photo/2020/09/18/19/31/laptop-5582775_960_720.jpg',
+    },
+    {
+      "id": 2,
+      "title": "Item 2",
+      "image":
+          'https://cdn.pixabay.com/photo/2020/09/18/19/31/laptop-5582775_960_720.jpg',
+    },
+    {
+      "id": 3,
+      "title": "Item 3",
+      "image":
+          'https://media.istockphoto.com/photos/woman-kayaking-in-fjord-in-norway-picture-id1059380230?b=1&k=6&m=1059380230&s=170667a&w=0&h=kA_A_XrhZJjw2bo5jIJ7089-VktFK0h0I4OWDqaac0c=',
+    },
+    {
+      "id": 4,
+      "title": "Item 4",
+      "image":
+          'https://cdn.pixabay.com/photo/2019/11/05/00/53/cellular-4602489_960_720.jpg',
+    },
+    {
+      "id": 5,
+      "title": "Item 5",
+      "image":
+          'https://cdn.pixabay.com/photo/2017/02/12/10/29/christmas-2059698_960_720.jpg',
+    },
+    {
+      "id": 6,
+      "title": "Item 6",
+      "image":
+          'https://cdn.pixabay.com/photo/2020/01/29/17/09/snowboard-4803050_960_720.jpg',
+    },
+    {
+      "id": 7,
+      "title": "Item 7",
+      "image":
+          'https://cdn.pixabay.com/photo/2020/02/06/20/01/university-library-4825366_960_720.jpg',
+    },
+    {
+      "id": 8,
+      "title": "Item 8",
+      "image":
+          'https://cdn.pixabay.com/photo/2020/11/22/17/28/cat-5767334_960_720.jpg',
+    },
+    {
+      "id": 9,
+      "title": "Item 9",
+      "image":
+          'https://cdn.pixabay.com/photo/2020/12/13/16/22/snow-5828736_960_720.jpg',
+    },
+    {
+      "id": 10,
+      "title": "Item 10",
+      "image":
+          'https://cdn.pixabay.com/photo/2020/12/09/09/27/women-5816861_960_720.jpg',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MasonryGridView.count(
-      itemCount: _items.length,
+      itemCount: _items2.length,
       padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
       crossAxisCount: 2,
       mainAxisSpacing: 4,
       crossAxisSpacing: 4,
       itemBuilder: (context, index) {
         return Card(
-          color: Color.fromARGB(Random().nextInt(256), Random().nextInt(256),
-              Random().nextInt(256), Random().nextInt(256)),
-          key: ValueKey(_items[index]['id']),
+          key: ValueKey(_items2[index]['id']),
           child: SizedBox(
-            height: 200.0, // _items[index]['height'],
-            child: Center(
-              child: Text(_items[index]['title']),
+            height: 220.0, // _items[index]['height'],
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  FadeInImage.memoryNetwork(
+                    placeholder: kTransparentImage,
+                    image: _items2[index]['image'],
+                    height: 150,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    alignment: Alignment.bottomLeft,
+                    child: Text(
+                      _items2[index]['title'],
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         );
